525c4d661e6aa284e360cad46bcba56f5b39e432
smnet97 <smnet97@gmail.com>
2023-08-20 12:59:24 +0500

    remove element

--- problems/python/RemoveElement/remove-element.py
+++ problems/python/RemoveElement/remove-element.py
@@ -1,7 +1,5 @@
 class Solution:
     def removeElement(self, nums: list, val: int) -> int:
-        if len(nums) == 0: return 0
-        elif len(nums) == 1: return 1
         
         t = 0
         for i in range(len(nums)):
